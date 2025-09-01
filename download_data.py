e04c6e9d9d1155b6f4bd746cf25fe74648d5957a
Saransh Baid <135456037+Saraid10@users.noreply.github.com>
2025-09-02 00:14:09 +0530

    Delete download_data.py

--- download_data.py
+++
@@ -1,8 +0,0 @@
-import nltk
-
-print("--- Starting NLTK data download ---")
-print("Downloading 'punkt' package...")
-nltk.download('punkt')
-print("\nDownloading 'stopwords' package...")
-nltk.download('stopwords')
-print("\n--- Download process finished ---")
