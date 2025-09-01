982a5820d1f6d641563fdf16dd48b63376d98329
Saransh Baid <135456037+Saraid10@users.noreply.github.com>
2025-09-02 00:14:27 +0530

    Delete find_path.py

--- find_path.py
+++
@@ -1,9 +0,0 @@
-import nltk
-import os
-
-# Get NLTK's default download directory
-path = nltk.downloader.Downloader().default_download_dir()
-print(f"NLTK's data directory is: {path}")
-
-# Create the directory if it doesn't exist
-os.makedirs(path, exist_ok=True)
